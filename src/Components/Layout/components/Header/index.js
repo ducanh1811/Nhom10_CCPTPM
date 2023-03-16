71a961a6bd51d7162f5e3133c2de6645aea3f3c2
ducanh1811 <119037853+ducanh1811@users.noreply.github.com>
2023-03-17 00:17:37 +0700

    Day 2 - DucAnh - XayDung Header P2

--- src/Components/Layout/components/Header/index.js
+++ src/Components/Layout/components/Header/index.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
+    faPersonRifle as user,
     faCircleXmark as close,
     faSpinner as loading,
     faMagnifyingGlass as search,
@@ -12,7 +13,6 @@ import images from '~/assets/images';
 import { Wrapper as PopperWrapper } from '~/Components/Popper';
 
 const cx = classNames.bind(styles);
-
 function Header() {
     const [searchResult, setSearchResult] = useState([]);
 
@@ -31,60 +31,48 @@ function Header() {
                         window.location.href = '/';
                     }}
                 >
-                    <img src={images.logo} alt="TikTok" />
+                    <img src={images.logo} alt="talkdesk" />
                 </div>
-
-                <Tippy
-                    interactive={true}
-                    visible={searchResult.length > 0}
-                    render={(attrs) => (
-                        <div
-                            className={cx('search-result')}
-                            tabIndex="-1"
-                            {...attrs}
-                        >
-                            <PopperWrapper>Ketqua</PopperWrapper>
-                        </div>
-                    )}
+                <div
+                    className={cx('menu')}
+                    onMouseEnter={() => {
+                        console.log('Enter');
+                    }}
                 >
-                    <div className={cx('search')}>
-                        <input
-                            type="text"
-                            placeholder="Search accounts and videos"
-                            spellCheck="false"
-                        />
-                        {/* <button className={cx('clear')}>
-                        <FontAwesomeIcon icon={close} />
-                    </button> */}
-                        <FontAwesomeIcon
-                            className={cx('loading')}
-                            icon={loading}
-                        />
-
-                        <button className={cx('search-btn')}>
-                            <FontAwesomeIcon icon={search} />
-                        </button>
-                    </div>
-                </Tippy>
+                    <ul>PRODUCTS</ul>
+                    <ul>SOLUTIONS</ul>
+                    <ul>CUSTOMERS</ul>
+                    <ul>SUPPORT</ul>
+                    <ul>RESOURCES</ul>
+                    <ul>ABOUT</ul>
+                </div>
                 <div className={cx('actions')}>
-                    <div className="container--right">
-                        <a
-                            href="https://account.talkdeskid.com/select/cc"
-                            className="profile__open"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            title="Talkdesk Enterprise Platform Login page"
-                        >
-                            <i className="icon-ui-profile -size-24"></i>
-                        </a>
-                        <button
-                            className="text globalsearch__open"
-                            title="Search"
-                            type="button"
-                        >
-                            <i className="icon-ui-search -size-24"></i>
-                        </button>
-                    </div>
+                    <a
+                        href="https://www.talkdesk.com/login"
+                        className={cx('login')}
+                        title="Talkdesk Enterprise Platform Login page"
+                    >
+                        {' '}
+                        <FontAwesomeIcon
+                            style={{
+                                fontSize: '20px',
+                                color: '#5405bd',
+                                margin: '10px',
+                            }}
+                            icon={user}
+                        />{' '}
+                    </a>
+                    <button className={cx('search')} title="Search">
+                        {' '}
+                        <FontAwesomeIcon
+                            style={{
+                                fontSize: '20px',
+                                color: '#5405bd',
+                                margin: '10px',
+                            }}
+                            icon={search}
+                        />{' '}
+                    </button>
                 </div>
             </div>
         </header>
