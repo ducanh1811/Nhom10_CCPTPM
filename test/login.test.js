d545a8f5544888bf70d285b906e9a76a11aa42c4
Võ Hồng Khang <87598819+VoHongKhang@users.noreply.github.com>
2023-04-21 23:55:29 +0700

    Update login.test.js

--- test/login.test.js
+++ test/login.test.js
@@ -1,29 +1,38 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
-const { before } = require('@jest/globals');
-const { after } = require('@jest/globals');
-const { expect } = require('@jest/globals');
+const { beforeAll, afterAll, test } = require('@jest/globals');
 
-const chromedriver = require('chromedriver');
+jest.setTimeout(100000); // timeout toàn cục
 
-describe('Login', function () {
-    let driver;
+let driver;
 
-    before(async function () {
-        driver = await new Builder().forBrowser('chrome').build();
-    });
+beforeAll(async () => {
+    driver = await new Builder().forBrowser('chrome').build();
+});
 
-    it('should login with valid credentials', async function () {
-        await driver.get('http://localhost:3000/login');
-        await driver.findElement(By.name('username')).sendKeys('ducanh');
-        await driver
-            .findElement(By.name('password'))
-            .sendKeys('anh181102', Key.RETURN);
-        await driver.wait(until.titleIs('Home Page'), 10000);
-        const url = await driver.getCurrentUrl();
-        expect(url).toBe('http://localhost:3000/home');
-    });
+afterAll(async () => {
+    await driver.quit();
+});
 
-    after(async function () {
-        await driver.quit();
-    });
+test('Login Test', async () => {
+    jest.setTimeout(100000);
+    // Navigate to the login page
+    await driver.get('http://localhost:3000/login');
+
+    // Find the email and password input fields, and the submit button
+    const emailField = await driver.findElement(By.id('username'));
+    const passwordField = await driver.findElement(By.id('password'));
+    const submitButton = await driver.findElement(By.id('login-button'));
+
+    // Enter the email and password, and click the submit button
+    await emailField.sendKeys('ducanh');
+    await passwordField.sendKeys('anh181102');
+    await submitButton.click();
+
+    // Wait for the page to load
+
+    await driver.wait(until.urlContains('/dashboard'), 100000);
+
+    // Check if the page contains the dashboard header
+    const header = await driver.findElement(By.css('h1.dashboard-header'));
+    expect(await header.getText()).toEqual('Dashboard');
 });
